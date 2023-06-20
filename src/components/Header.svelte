ed362c50106cd87556c3667687cadcc77bfd2671
zelpha-5W1H <tharunkothari@yahoo.com>
2023-06-20 13:12:11 +0530

    Header file done (not responsive yet)

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	const headers = ['Home', 'Collection', 'About'];
+	const handleClick = (header: string) => {
+		dispatch('headerClick', { header });
+	};
+</script>
+
+<div
+	class="flex fixed w-full h-min items-center bg-gray-950 opacity-90 border-b-4 border-purple-500 shadow-glow-purple"
+>
+	<div class="text-3xl text-rose-500 mx-3 my-2">
+		<img src="look_logo.svg" alt="logo" height="100" width="100" />
+	</div>
+	<div class="flex space-x-5 text-xl mx-2">
+		{#each headers as header}
+			<button
+				class="text-white hover:font-bold hover:text-purple-400"
+				on:click={() => handleClick(header)}>{header}</button
+			>{/each}
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+
+	const handleHeaderClick = (event: any) => {
+		console.log(event.detail.header);
+	};
+</script>
+
+<Header on:headerClick={handleHeaderClick} />
+<div class="h-screen w-screen bg-slate-950" />
+<Footer />
